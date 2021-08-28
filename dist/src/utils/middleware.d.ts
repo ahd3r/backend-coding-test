ba395b2f5754cb1e2ffc7ddfaa81148c56fb8dcb
ahd3r <ander11110@gmail.com>
2021-08-28 08:48:15 +0300

    remove dist from git

--- dist/src/utils/middleware.d.ts
+++
@@ -1,6 +0,0 @@
-import * as express from 'express';
-export declare const logReq: (req: express.Request, res: express.Response, next: express.NextFunction) => void;
-export declare const logRes: (resBody: any, method: string) => void;
-export declare const errorHandler: (err: any, req: express.Request, res: express.Response, next: express.NextFunction) => void;
-export declare const responseHandler: (req: express.Request, res: express.Response, next: express.NextFunction) => void;
-export declare const validateErrors: express.RequestHandler;
